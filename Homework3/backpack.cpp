9179305179394bf380149ab66f5e5a0c068e0ebe
Milen Temelkov <migete@abv.bg>
2024-11-25 14:06:40 +0200

    refactor(homework3): added mutation after selection

--- Homework3/backpack.cpp
+++ Homework3/backpack.cpp
@@ -10,30 +10,46 @@ struct Item {
     int value = 0;
 
     void print() const {
-        // cout << "weight: " << weight << " value: " << value << endl;
+        cout << "weight: " << weight << " value: " << value << endl;
     }
 };
 
 struct Chromosome {
     vector<bool> itemsIncluded;
     int fitness = 0;
+    int weight = 0;
 
     Chromosome() = default;
 
     Chromosome(int maxWeight, const vector<Item>& items, const vector<bool> itemsToInclude) : itemsIncluded(itemsToInclude) {
-        int currWeight = 0;
 
         for (int i = 0; i < items.size(); i++) {
             if (itemsIncluded[i]) {
                 fitness += items[i].value;
-                currWeight += items[i].weight;
+                weight += items[i].weight;
             }
 
-            if (currWeight > maxWeight) {
+            if (weight > maxWeight) {
                 fitness = 0;
                 break;
             }
         }
+    }
+
+    bool invert(int maxWeight, const vector<Item>& items, int item) {
+        itemsIncluded[item] = !itemsIncluded[item];
+
+        if (itemsIncluded[item]) {
+            fitness += items[item].value;
+            weight += items[item].weight;
+
+            fitness = weight > maxWeight ? 0 : fitness;
+        } else {
+            fitness -= items[item].value;
+            weight -= items[item].weight;
+        }
+
+        return fitness;
     }
 };
 
@@ -59,37 +75,17 @@ bool compareChromosomesFitness(const Chromosome& lhs, const Chromosome& rhs) {
     return lhs.fitness > rhs.fitness; 
 }
 
-// bool compareItemsByWeightAndValue(const Item& lhs, const Item& rhs) {
-//     return lhs.weight < rhs.weight ? true : lhs.value < rhs.value;
-// }
-
 Chromosome generateRandomChromosome(int maxWeight, const vector<Item>& items) {
     vector<bool> itemsIncluded(items.size(), 0);
     random_device rd;
     mt19937 gen(rd());
-    
-    // uniform_int_distribution<> dist(0, 1);
-    
-    // int currWeight = 0;
-
-    // for (int i = 0; i < items.size(); i++) {
-    //     itemsIncluded[i] = dist(gen);
-
-    //     currWeight += itemsIncluded[i] ? items[i].weight : 0;
-        
-    //     if (currWeight > maxWeight) {
-    //         currWeight -= items[i].weight;
-    //         itemsIncluded[i] = 0;
-    //         break;
-    //     }
-    // }
 
     uniform_int_distribution<> dist(0, items.size() - 1);
     int currWeight = 0;
 
     while (true) {
         int index = dist(gen);
-        if (items[index].weight + currWeight > maxWeight) {
+        if (itemsIncluded[index] != 1 && items[index].weight + currWeight > maxWeight) {
             break;
         }
 
@@ -123,10 +119,6 @@ Chromosome crossbreed(int maxWeight, const vector<Item>& items, const Chromosome
     for (int i = 0; i < items.size(); i++) {
 
         childItems[i] = (rand() % 2) ? parent1.itemsIncluded[i] : parent2.itemsIncluded[i];
-       
-        if (rand() % 100 < 5) {
-            childItems[i] = !childItems[i];
-        }
 
         currWeight += childItems[i] ? items[i].weight : 0;
         
@@ -141,7 +133,6 @@ Chromosome crossbreed(int maxWeight, const vector<Item>& items, const Chromosome
 }
 
 vector<Chromosome> makeCrossoverOfPopulation(int maxWeight, const vector<Item>& items, const vector<Chromosome>& population) {
-    // cout << "CROSSOVER" << endl;
     vector<Chromosome> newPopulation(population.size());
     random_device rd;
     mt19937 gen(rd());
@@ -159,23 +150,9 @@ vector<Chromosome> makeCrossoverOfPopulation(int maxWeight, const vector<Item>& 
     return newPopulation;
 }
 
-vector<Chromosome> selectivePurge(vector<Chromosome>& parentPopulation, vector<Chromosome>& breededPopulation) {
-    // cout << "PURGE" << endl;
+vector<Chromosome> selectivePurge(const vector<Chromosome>& randomPopulation, const vector<Chromosome>& parentPopulation, const vector<Chromosome>& breededPopulation) {
     vector<Chromosome> resultPopulation(parentPopulation.size());
-
-    // int parentIndex = 0, breededIndex = 0;
-
-    // int oneTenth = parentPopulation.size() / 10;
-
-    // for (int i = 0; i < oneTenth; i++) {
-    //     resultPopulation[i] = parentPopulation[i];
-    // }
-
-    // resultPopulation[0] = parentPopulation[0];
-
-    // for (int i = 1; i < parentPopulation.size(); i++) {
-    //     resultPopulation[i] = breededPopulation[i - 1];
-    // }
+    
     int parentIndex = 0, breededIndex = 0;
 
     for (int i = 0; i < parentPopulation.size(); i++) {
@@ -187,16 +164,33 @@ vector<Chromosome> selectivePurge(vector<Chromosome>& parentPopulation, vector<C
     return resultPopulation;
 }
 
+void mutate(int maxWeight, const vector<Item>& items, vector<Chromosome>& population, int fromWhere) {
+    for (int i = fromWhere; i < population.size(); i++) {
+        for (int j = 0; j < population[i].itemsIncluded.size(); j++) {
+            if (!(rand() % 100)) {
+                if (!population[i].invert(maxWeight, items, j)) {
+                    population[i].invert(maxWeight, items, j);
+                }
+            }
+        }
+    }
+}
+
 void solveKnapsack(int maxWeight, const vector<Item>& items, int generations, int populationSize) {
     vector<Chromosome> population = buildRandomPopulation(maxWeight, items, populationSize);
 
     for (int generation = 0; generation < generations; generation++) {
         
-        cout << "Generation " << generation + 1 << ": Best fitness = " << population[0].fitness << endl;
-        cout << "Generation " << generation + 1 << ": Second fitness = " << population[1].fitness << endl << endl;
-        vector<Chromosome> newPopulation = makeCrossoverOfPopulation(maxWeight, items, population);
+        if (!(generation % 8)) {
+            cout << "Generation " << generation + 1 << ": Best fitness = " << population[0].fitness << endl;
+        }
 
-        population = selectivePurge(population, newPopulation);
+        vector<Chromosome> breededPopulation = makeCrossoverOfPopulation(maxWeight, items, population);
+        vector<Chromosome> randomPopulation = buildRandomPopulation(maxWeight, items, populationSize);
+
+        population = selectivePurge(randomPopulation, population, breededPopulation);
+
+        mutate(maxWeight, items, population, (population.size() / 10));
     }
 }
 
@@ -209,9 +203,6 @@ int main() {
         cin >> itemWeight >> itemValue;
         items[i] = Item( {itemWeight, itemValue } );
     }
-
-    // sort(items.begin(), items.end(), compareItemsByWeightAndValue);
-    // cout << "???" << endl;
 
     int generations = itemsCount * 2;
     int individuals = itemsCount;
